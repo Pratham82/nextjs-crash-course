e3d0b3b47b11165fd28747a5bf5630c37eda64ab
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-10-24 17:46:44 +0530

    feat: created navbar and backbutton

---
+++ components/backBtn.tsx
@@ -0,0 +1,7 @@
+export default function BackButton() {
+  return (
+    <h3>
+      &nbsp; &nbsp; <a href={'/'}>Go back</a>{' '}
+    </h3>
+  )
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,35 @@
+import styles from '../styles/Home.module.css'
+
+export default function Navbar() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100vw',
+        justifyContent: 'space-evenly',
+        boxShadow: ' 0px 3px 9px -4px rgba(0,0,0,0.75)',
+      }}
+    >
+      <h4 className={styles.title}>
+        <a href="/"> Next.js Demo </a>
+      </h4>
+      <ul>
+        {['/home', '/hello_world', '/about', '/contact'].map(link => (
+          <li
+            style={{
+              display: 'inline-block',
+              listStyle: 'none',
+              padding: '10px',
+              fontSize: '25px',
+              fontWeight: 'bold',
+            }}
+            key={link}
+          >
+            {' '}
+            <a href={link}> {link.replace('/', '')} </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}

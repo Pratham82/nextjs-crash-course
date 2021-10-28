a010c753154479c573fa6b7ee905fec984d2d96b
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-10-28 23:03:34 +0530

    feat: added custom styles options

--- pages/styling.tsx
+++ pages/styling.tsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+// 3. CSS modules
 import styles from '../styles/Home.module.css'
+import otherStyles from '../styles/styling.module.scss'
+
+// These styles are component level styles
+// To apply these styles globally, add global after jsx in style tag
+const StyledJSX = () => (
+  <div>
+    <h2 className="title">Styles JSX</h2>
+    <style jsx>{`
+      .title {
+        color: red;
+        text-align: center;
+      }
+    `}</style>
+  </div>
+)
+
+// 2. For adding styles to all components, import a css file in the _app.tsx
+// 4. For adding sass in the app, install sass. And change the extension of css module to scss 
+
 export default function Styles() {
   const typesOfStyling = [
     'styledJSX',
@@ -15,6 +35,8 @@ export default function Styles() {
       {typesOfStyling.map((style, i) => (
         <h3 key={i} className={styles.centeredText}>{`${i + 1}. ${style}`}</h3>
       ))}
+      <StyledJSX />
+      <h2 className={otherStyles.title}>Test</h2>
     </>
   )
 }

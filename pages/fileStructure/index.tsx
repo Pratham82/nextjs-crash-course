bb1394771de5d1d92a57fcbb77310cca2301c1ec
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-10-28 23:03:55 +0530

    feat: added file structure component

---
+++ pages/fileStructure/index.tsx
@@ -0,0 +1,9 @@
+import styles from './filestructure.module.scss'
+
+export default function FileStructure() {
+  return (
+    <>
+      <h2 className={styles.title}>File Structure</h2>
+    </>
+  )
+}

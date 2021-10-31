3cf3dc0f599efd4e85692244067045baa6c750dc
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-11-01 01:58:45 +0530

    feat: Added login page link on homepage

--- pages/home.tsx
+++ pages/home.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
     <>
       <Navbar />
       <h1 className={styles.title}>HomePage</h1>
-      {['/home', '/hello_world', '/styling', '/fileStructure'].map(
+      {['/home', '/hello_world', '/styling', '/fileStructure', '/login'].map(
         (page, i) => (
           <h3 className={styles.centeredText} key={page}>
             <a href={page}>

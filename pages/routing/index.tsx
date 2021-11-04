bcfda9a1eed135b4749f65ef54c4482b42689e0f
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-11-04 22:11:28 +0530

    feature: added routing

---
+++ pages/routing/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import Navbar from '../../components/navbar'
+import styles from '../../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function () {
+  return (
+    <>
+      <Navbar />
+      <h1 className={styles.centeredText}>Routing</h1>
+      <h2 className={styles.centeredText}>Route 1</h2>
+      <h2 className={styles.centeredText}>Route 2</h2>
+      <Link href="/routing/nested_route">
+        <a href="">
+          <h2 className={styles.centeredText}>Nested Route 🔗</h2>
+        </a>
+      </Link>
+    </>
+  )
+}

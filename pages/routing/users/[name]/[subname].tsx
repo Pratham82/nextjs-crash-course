84c130e029d7344a98f54cc89b23d4d0ef4ae3e4
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-11-25 23:56:50 +0530

    feat: added userRouter example

--- pages/routing/users/[name]/[subname].tsx
+++ pages/routing/users/[name]/[subname].tsx
@@ -1,12 +1,34 @@
+import { useRouter } from 'next/router'
 import * as React from 'react'
 import Navbar from '../../../../components/navbar'
 import styles from '../../../../styles/Home.module.css'
+import userStyles from '../../users/Users.module.scss'
 
 export default function () {
+  const router = useRouter()
+  console.log(router)
+  const {
+    query: { name, subname },
+    asPath,
+  } = router
   return (
     <>
       <Navbar />
       <h1 className={styles.centeredText}>Subname</h1>
+      <p className={styles.centeredText} style={{ fontSize: '20px' }}>
+        Entered Query: <br /> <br />
+        name: <b>{name}</b> , subName : <b>{subname}</b>
+        <br />
+        <br />
+        Current route: {asPath}
+        <br />
+        <button
+          className={userStyles.routeBtn}
+          onClick={() => router.push('/home')}
+        >
+          Go to Home
+        </button>
+      </p>
     </>
   )
 }
